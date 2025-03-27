d4a5e53ff863db1001f94584aa383579ab7bf10a
Laharika Bandaru <laharika573@gmail.com>
2025-03-27 17:53:55 +0530

    message

---
+++ array.js
@@ -0,0 +1,26 @@
+const number=[1,2,3,4,5,[5,6],[8,9]]
+console.log(number);
+
+// accessing array elements
+console.log(number[1]);
+console.log(number[5]);
+
+// constructor 
+const numbers = new Array(1, 2, 3, 4, 5);
+console.log(numbers);
+
+// Insertion at End 
+number.push(7);
+number.push(11);
+console.log(number);
+
+// insertion at start
+const fruits = ['apple', 'banana', 'orange'];
+
+fruits.unshift('kiwi');
+fruits.unshift('grapes');
+
+console.log(fruits); 
+
+// Insertion at Random Index
+
