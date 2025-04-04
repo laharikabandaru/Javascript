c7bffff570cf68982c7f2bbc240af2438c5f3f0b
Laharika Bandaru <laharika573@gmail.com>
2025-04-04 21:24:37 +0530

    promises

---
+++ advpromise.js
@@ -0,0 +1,7 @@
+const promise1 = new Promise((resolve, reject)=>{setTimeout(()=>resolve('First'),1000)});
+const promise2 = new Promise((resolve, reject) =>{setTimeout(()=>reject('second'), 2000)});
+const promise3 = new Promise((resolve,reject)=>{setTimeout(()=>resolve('third'),3000)});
+Promise.all([promise1,promise2,promise3]).then((value) =>{console.log(value)}) .catch((error) => {console.log(error)});
+Promise.any([promise1,promise2,promise3]).then((value) =>{console.log(value)}) .catch((error) => {console.log(error)});
+Promise.allSettled([promise1,promise2,promise3]).then((value) =>{console.log(value)}) .catch((error) => {console.log(error)});
+Promise.race([promise1,promise2,promise3]).then((value) =>{console.log(value)}) .catch((error) => {console.log(error)});
